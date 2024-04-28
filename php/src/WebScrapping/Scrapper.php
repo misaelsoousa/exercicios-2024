d75fe59ba7245ad2348d99ab7cc61dd6af3dc6eb
misaelsoousa <misasoousa@gmail.com>
2024-04-28 19:37:36 -0300

    Criado lógica para WebScrapping

--- php/src/WebScrapping/Scrapper.php
+++ php/src/WebScrapping/Scrapper.php
@@ -4,6 +4,7 @@ namespace Chuva\Php\WebScrapping;
 
 use Chuva\Php\WebScrapping\Entity\Paper;
 use Chuva\Php\WebScrapping\Entity\Person;
+use Box\Spout\Writer\Common\Creator\WriterEntityFactory;
 
 /**
  * Does the scrapping of a webpage.
@@ -14,17 +15,73 @@ class Scrapper {
    * Loads paper information from the HTML and returns the array with the data.
    */
   public function scrap(\DOMDocument $dom): array {
-    return [
-      new Paper(
-        123,
-        'The Nobel Prize in Physiology or Medicine 2023',
-        'Nobel Prize',
-        [
-          new Person('Katalin Karikó', 'Szeged University'),
-          new Person('Drew Weissman', 'University of Pennsylvania'),
-        ]
-      ),
-    ];
+    $data = $this->scrapFromHTML($dom);
+
+    $this->writeToXLSX($data);    
+
+    return [];
   }
 
+  public function scrapFromHTML(\DOMDocument $dom): array {
+    // Cria um novo objeto DOMXPath
+    $xpath = new \DOMXPath($dom);
+    // Seleciona todos os links dentro da terceira seção com a classe especificada
+    $links = $xpath->query('//a[contains(@class, "paper-card")]');
+
+    $data = [];
+
+    foreach ($links as $link) {
+      $id = $xpath->query('.//div[contains(@class, "volume-info")]', $link);
+      $title = $xpath->query('.//h4[contains(@class, "paper-title")]', $link);
+      $type = $xpath->query('.//div[contains(@class, "tags mr-sm")]', $link);
+
+      $authors_container = $xpath->query('.//div[contains(@class, "authors")]', $link)[0];
+      $authors = $xpath->query('.//span', $authors_container);
+
+      $authors_array = [];
+
+      foreach ($authors as $author) {
+
+        $name = $author->textContent;
+    
+        $institution = $author->getAttribute("title");
+        
+        $person = new Person($name, $institution);
+        array_push($authors_array, $person);
+    }
+
+      $paper = new Paper(
+        $id[0]->textContent,
+        $title[0]->textContent,
+        $type[0]->textContent,
+        $authors_array
+      );
+     array_push($data, $paper);
+    }
+
+    print_r($data);
+
+    // new Paper(0, "", "", $authors_array);
+
+    // Inicializa o array para armazenar os resultados
+    $result = [];
+
+    // Itera sobre os links encontrados
+    // foreach ($links as $link) {
+    //     // Extrai o título do elemento <h4> dentro do link atual
+    //     $title = $link->getElementsByTagName("h4")->item(0)->textContent;
+        
+    //     // Adiciona o título ao resultado
+    //     $result[] = $title;
+    // }
+
+    // Retorna o array de resultados
+    return $result;
+}
+
+
+  public function writeToXLSX($data): array {
+    
+    return [];
+  }
 }
