e5abeb8181f309c9666ec09de77688fe11e93dcf
misaelsoousa <misasoousa@gmail.com>
2024-05-03 15:20:40 -0300

    Criado cabeçalho e Slide-Menu

--- ts/src/app/app.module.ts
+++ ts/src/app/app.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SlideMenuComponent } from './slide-menu/slide-menu.component';
+import { HeaderComponent } from './header/header.component';
+import { ContentComponent } from './content/content.component';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    SlideMenuComponent,
+    HeaderComponent,
+    BrowserModule,
+    ContentComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
